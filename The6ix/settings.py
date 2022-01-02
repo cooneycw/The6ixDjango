dc1b868a63547792301621e3fe4e4e3bcd0a5a7c
cooneycw <cooneycw@icloud.com>
2022-01-02 15:34:00 -0500

    Implemented Amazon Secrets Manager framework.

--- The6ix/settings.py
+++ The6ix/settings.py
@@ -11,22 +11,20 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import environ
+from .secrets import get_secrets
+import ast
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-# reading .env file
-environ.Env.read_env()
-
-
+secret = get_secrets()
+secret_dict = ast.literal_eval(secret)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = secret_dict['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,11 +82,11 @@ WSGI_APPLICATION = 'The6ix.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("POSTGRES_DB"),
-        'USER': env("POSTGRES_USER"),
-        'PASSWORD': env("POSTGRES_PASSWORD"),
-        'HOST': env("POSTGRES_HOST"),
-        'PORT': env("POSTGRES_PORT"),
+        'NAME': secret_dict['POSTGRES_DB'],
+        'USER': secret_dict['POSTGRES_USER'],
+        'PASSWORD': secret_dict['POSTGRES_PASSWORD'],
+        'HOST': secret_dict['POSTGRES_HOST'],
+        'PORT': secret_dict['POSTGRES_PORT'],
     }
 }
 
