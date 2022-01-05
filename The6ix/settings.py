f812f717381240065f934f9fdab7118fbd413bc9
cooneycw <cooneycw@icloud.com>
2022-01-05 18:00:54 -0500

    Updated style sheets + modified messages.

--- The6ix/settings.py
+++ The6ix/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from .secrets import get_secrets
 import ast
 import socket
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +51,7 @@ CSRF_TRUSTED_ORIGINS = ["https://the6ixclan.ca"]
 INSTALLED_APPS = [
     'The6ixDjango.apps.The6IxdjangoConfig',
     'users.apps.UsersConfig',
+    'clashstats.apps.ClashstatsConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -152,7 +154,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "mediafiles"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -161,5 +163,4 @@ LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
