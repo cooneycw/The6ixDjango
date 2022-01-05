b20e8fff7c8f1a3d20a99b1b08e17a8513cbfb6f
cooneycw <cooneycw@icloud.com>
2022-01-04 19:55:59 -0500

    Correcting navbar.

--- The6ixDjango/urls.py
+++ The6ixDjango/urls.py
@@ -3,5 +3,5 @@ from The6ixDjango import views
 
 urlpatterns = [
     path('', views.home, name='The6ixDjango-home'),
-    path('about/', views.about, name='The6ixDjango-about')
+
 ]
--- The6ixDjango/views.py
+++ The6ixDjango/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'The6ixDjango/home.html', {'title': 'The6ixClan: Home'})
-
-
-def about(request):
-    return render(request, 'The6ixDjango/about.html', {'title': 'The6ixClan: About'})
