7872f6ba6d2b71e39d9041b115f5c73b30dcfba0
Christopher Cooney <cooneycw@icloud.com>
2022-06-13 09:22:17 -0400

    Correct celery error.

--- clashstats/tasks.py
+++ clashstats/tasks.py
@@ -5,19 +5,27 @@ from The6ix.celery import app
 from .Tasks_Scoring.score import deck_analyzer
 
 
+# @app.task(name='clashstats.tasks.scoring')
+# def analyze_deck(game_df):
+#     results = None
+#     timeout = (60 * 20)
+#     have_lock = False
+#     my_lock = REDIS_INSTANCE.lock(REDIS_LOCK_KEY, timeout=timeout)
+#     while not have_lock:
+#         have_lock = my_lock.acquire(blocking=False)
+#         if have_lock:
+#             print('start deck analysis')
+#             results = deck_analyzer(game_df)
+#         else:
+#             print('waiting for lock to commence...')
+#             time.sleep(10)
+#     my_lock.release()
+#     return results
+
+
 @app.task(name='clashstats.tasks.scoring')
 def analyze_deck(game_df):
     results = None
-    timeout = (60 * 20)
-    have_lock = False
-    my_lock = REDIS_INSTANCE.lock(REDIS_LOCK_KEY, timeout=timeout)
-    while not have_lock:
-        have_lock = my_lock.acquire(blocking=False)
-        if have_lock:
-            print('start deck analysis')
-            results = deck_analyzer(game_df)
-        else:
-            print('waiting for lock to commence...')
-            time.sleep(10)
-    my_lock.release()
+    print('start deck analysis')
+    results = deck_analyzer(game_df)
     return results
